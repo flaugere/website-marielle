d3b14925762323f3f61b43d28ec595fa1c937213
David FLAUGERE <flaugere.david@gmail.com>
2025-09-19 06:57:56 +0200

    feat(site): first commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
+import icon from 'astro-icon'
+import rehypeFigureTitle from 'rehype-figure-title'
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://mariellepousse.fr',
+	base: '/',
+	integrations: [mdx(), sitemap(), icon()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime, remarkModifiedTime],
+		rehypePlugins: [rehypeFigureTitle, rehypeAccessibleEmojis],
+	},
+})
---
+++ src/consts.ts
@@ -0,0 +1,45 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Marielle POUSSE - Thérapeute'
+export const SITE_DESCRIPTION =
+	'Thérapie Psycho-corporelle avec méthode neuro-émotionnelle'
+
+// If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
+// e.g. '/blog' if you are deploying to example.com/blog
+// Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
+export const SITE_BASE = ''
+
+export const SocialLinks = [
+	{
+		name: 'Instagram',
+		url: 'https://instagram.com/mpousse',
+	},
+]
+
+export const WebsiteLinks = [
+	{
+		name: 'Accueil',
+		url: '',
+	},
+	{
+		name: 'La méthode',
+		url: '#methode',
+	},
+	{
+		name: 'La praticienne',
+		url: '#praticienne',
+	},
+	{
+		name: 'Les patient.e.s',
+		url: '#patientes',
+	},
+	{
+		name: 'Les pratiques',
+		url: '#pratiques',
+	},
+	{
+		name: 'Contact',
+		url: '#contact',
+	},
+]
